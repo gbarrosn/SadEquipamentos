81469f8e04eb20926300e72f6391a8f06ba24a66
gbarrosn <gbarros.n@gmail.com>
2023-09-21 12:02:11 -0300

    testando os dados do usuário para depois escrever o pdf

--- src/RelatoriosSAD/FormRelatorioTermoUsuario.java
+++ src/RelatoriosSAD/FormRelatorioTermoUsuario.java
@@ -11,20 +11,33 @@ public class FormRelatorioTermoUsuario {
 
     public void gerarRelatorio() {
         try {
-/*
+
             // use Usuario object to load the daata
 
             String nome = usuario.getNome();
             String CESU = String.valueOf(usuario.getEtiquetaCESU());
-            String tomboMicro = String.valueOf(usuario.getTombamentoMicro());
+            String tomboMicro = ManipulaNumero.manipularNumero((int) usuario.getTombamentoMicro());
+            String tomboMonitor = ManipulaNumero.manipularNumero((int) usuario.getTombamentoMonitor());
+            String tomboMonitor1 = ManipulaNumero.manipularNumero((int) usuario.getTombamentoMonitor1());
+            String serieMicro = usuario.getSerieMicro();
+            String serieMonitor = usuario.getSerieMonitor();
+            String serieMonitor1 = usuario.getSerieMonitor1();
+            String modeloMicro = usuario.getModeloMicro().getModelo();
 
-*/
-            String tomboMicro = String.valueOf((usuario.getTombamentoMicro()));
+            System.out.println("Nome: " + nome);
+            System.out.println("CESU: " + CESU);
+            System.out.println("Tombo Micro: " + tomboMicro);
+            System.out.println("Tombo Monitor: " + tomboMonitor);
+            System.out.println("Tombo Monitor 1: " + tomboMonitor1);
+            System.out.println("Série Micro: " + serieMicro);
+            System.out.println("Série Monitor: " + serieMonitor);
+            System.out.println("Série Monitor 1: " + serieMonitor1);
+            System.out.println("Modelo Micro: " + modeloMicro);
+
+
         } catch (Exception e) {
 
         }
-
-    // use the tombamento of the user to return a tombo starting with 120101.
 
     }
 
