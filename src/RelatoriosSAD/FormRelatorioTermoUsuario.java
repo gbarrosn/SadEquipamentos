9e06cceed929c5c9582b3f90ad407fd7764fa4d5
gbarrosn <gbarros.n@gmail.com>
2023-09-21 11:54:45 -0300

    feita a classe para tratar o tombo

--- src/RelatoriosSAD/FormRelatorioTermoUsuario.java
+++ src/RelatoriosSAD/FormRelatorioTermoUsuario.java
@@ -19,7 +19,7 @@ public class FormRelatorioTermoUsuario {
             String tomboMicro = String.valueOf(usuario.getTombamentoMicro());
 
 */
-            String tomboMicro = manipularNumero(usuario.getTombamentoMicro());
+            String tomboMicro = String.valueOf((usuario.getTombamentoMicro()));
         } catch (Exception e) {
 
         }
@@ -29,37 +29,4 @@ public class FormRelatorioTermoUsuario {
     }
 
 
-}
-
-
-
-public static String manipularNumero(long numero) {
-    // Transforma o número em uma string
-    String numeroStr = String.valueOf(numero);
-
-    // Conta quantos dígitos possui
-    int qtdDigitos = numeroStr.length();
-
-    // Adiciona '120101.' no início da string
-    numeroStr = "120101." + numeroStr;
-
-    // Separa a string em três partes
-    String parte1 = numeroStr.substring(0, 7);  // '120101.'
-    String parte2 = numeroStr.substring(7, 7 + qtdDigitos);  // Dígitos no meio
-    String parte3 = numeroStr.substring(7 + qtdDigitos);  // Últimos 4 dígitos
-
-    // Completa os dígitos no meio com zeros à esquerda
-    while (parte2.length() < 6) {
-        parte2 = "0" + parte2;
-    }
-
-    // Retorna a string completa
-    return parte1 + parte2 + parte3;
-}
-
-public static void main(String[] args) {
-    int numero = 1234567890;
-    String resultado = manipularNumero(numero);
-    System.out.println(resultado);
-}
 }
