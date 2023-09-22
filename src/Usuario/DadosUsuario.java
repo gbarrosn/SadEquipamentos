082e4e93343987e2e89280f227006171b33ca83d
gabriel.barrosn <gbarros.n@gmail.com>
2023-09-22 11:14:29 -0300

    erro na query alterar resolvido

--- src/Usuario/DadosUsuario.java
+++ src/Usuario/DadosUsuario.java
@@ -141,8 +141,8 @@ public class DadosUsuario extends ConectarSQL implements InterfaceUsuario {
             sql += ",id_marca_monitor2='"+alterarUser.getMarcaMonitor1().getIdMarca()+"',";
             sql += "id_proprietario_monitor2='"+alterarUser.getProprietarioMonitor1().getIdProprietario()+"',";
             sql += "tombo_monitor2='"+alterarUser.getTombamentoMonitor1()+"',";
-            sql += "serie_monitor2='"+alterarUser.getSerieMonitor1()+"'";
-            sql += "cpf='"+alterarUser.getCpf()+"'";
+            sql += "serie_monitor2='"+alterarUser.getSerieMonitor1()+"',";
+            sql += "cpf='"+alterarUser.getCpf()+"'"; //ERRO NA QUERY AQUI
 
 
             sql += " WHERE id_usuario=" + selecionadoUser.getId_usuario() + ";";
