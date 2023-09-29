de3650bd84e0bf9a8fde9f5dcacae0d63d317492
gabriel.barrosn <gbarros.n@gmail.com>
2023-09-29 11:41:34 -0300

    fix janela termo de devolução

--- src/RelatoriosSAD/JanelaSalvarDevolucao.java
+++ src/RelatoriosSAD/JanelaSalvarDevolucao.java
@@ -1,31 +1,27 @@
 package RelatoriosSAD;
-import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.plaf.FileChooserUI;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocument.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.plaf.FileChooserUI;
+
 public class JanelaSalvarDevolucao extends javax.swing.JFrame {
 
-    /**
-     * Creates new form JanelaSalvar
-     */
+    private PDDocument documento;
+
     public JanelaSalvarDevolucao() {
         initComponents();
     }
-
 
     final void icone() {
         BufferedImage img = null;
@@ -33,9 +29,9 @@ public class JanelaSalvarDevolucao extends javax.swing.JFrame {
             img = ImageIO.read(new File("resources\\favicon-cor.png"));
             this.setIconImage(img);
         } catch (IOException e) {
+            // Handle the exception appropriately
         }
     }
-    private PDDocument documento;
 
     public JanelaSalvarDevolucao(PDDocument documento, String nomeUsuario) {
         icone();
@@ -44,13 +40,9 @@ public class JanelaSalvarDevolucao extends javax.swing.JFrame {
         setTitle("Salvar Termo de Devolução em PDF");
         this.documento = documento;
 
-        String defaultFile="";
-        defaultFile = "Termo de Devolução - "+ nomeUsuario ;
-
+        String defaultFile = "Termo de Devolução - " + nomeUsuario;
 
         try {
-
-            JFileChooser janelaSalvarDevolucao = new JFileChooser();
             janelaSalvarDevolucao.setCurrentDirectory(new File("\\\\sadfs01\\GTINF\\USINF\\Sistema SAD Equipamentos\\Termos de Devolução\\"));
             FileChooserUI fcUi = janelaSalvarDevolucao.getUI();
             Class<? extends FileChooserUI> fcClass = fcUi.getClass();
@@ -60,13 +52,10 @@ public class JanelaSalvarDevolucao extends javax.swing.JFrame {
         } catch (Exception e) {
             JOptionPane.showMessageDialog(rootPane, "Erro - " + e.getMessage());
         }
-
     }
 
-
     private void initComponents() {
-
-        JFileChooser janelaSalvarDevolucao = new javax.swing.JFileChooser();
+        janelaSalvarDevolucao = new javax.swing.JFileChooser();
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -102,42 +91,31 @@ public class JanelaSalvarDevolucao extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void janelaSalvarDevolucaoPropertyChange(PropertyChangeEvent evt) {
     }
 
+    private void janelaSalvarDevolucaoPropertyChange(PropertyChangeEvent evt) {
+        // TODO add your handling code here
+    }
 
     private void janelaSalvarDevolucaoActionPerformed(ActionEvent evt) {
         try {
-
-            //JFileChooser input = new JFileChooser();
-            //int result = input.showSaveDialog(this);
-            String nomeDoArquivo;
-            nomeDoArquivo = janelaSalvarDevolucao.getSelectedFile() + ".pdf";
+            String nomeDoArquivo = janelaSalvarDevolucao.getSelectedFile() + ".pdf";
 
             if (!nomeDoArquivo.equals("null.pdf")) {
-
                 this.documento.save(nomeDoArquivo);
                 JOptionPane.showMessageDialog(rootPane, "Termo de Responsabilidade gerado com sucesso em:   \n" + "\"" + nomeDoArquivo + "\"");
                 dispose();
-
             } else if (nomeDoArquivo.equals("null.pdf")) {
-
                 dispose();
-
             }
 
         } catch (Exception e) {
             JOptionPane.showMessageDialog(rootPane, "Erro - " + e.getMessage());
         }
     }
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -154,9 +132,7 @@ public class JanelaSalvarDevolucao extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(JanelaSalvarDevolucao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new JanelaSalvarDevolucao().setVisible(true);
@@ -164,9 +140,5 @@ public class JanelaSalvarDevolucao extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JFileChooser janelaSalvarTermo;
-    // End of variables declaration//GEN-END:variables
-
-
+    private javax.swing.JFileChooser janelaSalvarDevolucao;
 }
